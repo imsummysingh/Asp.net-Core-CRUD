e932acf946be3c039e072eff558471798fc45c21
Summy Singh <summysingh86@gmail.com>
2024-07-29 19:25:14 +0530

    Solution added

---
+++ CRUDSolution/CRUD/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
